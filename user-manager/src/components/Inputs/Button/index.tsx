f3ced79de3de4ed4093607127a9585042fb95236
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-02 16:30:06 +0700

    Update button to match popover component #6

--- user-manager/src/components/Inputs/Button/index.tsx
+++ user-manager/src/components/Inputs/Button/index.tsx
@@ -10,6 +10,7 @@ type TButtonSize = 'sm' | 'md' | 'lg';
 interface IButtonProps {
   variants: TVariant;
   size: TButtonSize;
+  icon?: string;
   additionalClass?: string;
   children: ReactNode;
   onClick: () => void;
@@ -18,6 +19,7 @@ interface IButtonProps {
 const Button = ({
   variants,
   size,
+  icon,
   additionalClass,
   children,
   onClick
@@ -27,6 +29,12 @@ const Button = ({
       className={`btn btn-${variants} btn-${size} ${additionalClass}`}
       onClick={onClick}
     >
+      {icon &&
+        <img
+          className='btn-icon'
+          alt='icon'
+          src={icon}
+        />}
       {children}
     </button>
   );
