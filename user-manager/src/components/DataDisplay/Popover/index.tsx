de654edf671606a72f66d6f5f5f762e5a4b52da7
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-02 16:29:03 +0700

    Implement popover component #6

---
+++ user-manager/src/components/DataDisplay/Popover/index.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useRef, useState } from 'react';
+
+// CSS
+import '@components/DataDisplay/Popover/Popover.css';
+
+//Hooks
+import useBackDrop from '@hooks/useBackDrop';
+
+//Components
+import Button from '@components/Inputs/Button';
+
+interface IPopoverOption {
+  text: string;
+  onClick?: () => void;
+}
+
+interface IPopoverProps {
+  isOpen?: boolean;
+  icon: string;
+  children: string;
+  options: IPopoverOption[];
+  onOpenModal: () => void;
+}
+
+const Popover = ({
+  isOpen = false,
+  icon,
+  children = 'New',
+  options,
+  onOpenModal
+}: IPopoverProps) => {
+  const optionRef = useRef<HTMLDivElement | null>(null);
+  const [isShowOption, setShowOption] = useState(false);
+
+  const closeOption = () => {
+    setShowOption(false);
+  };
+
+  const handleTogglePopover = () => {
+    setShowOption(true);
+  };
+
+  useBackDrop({
+    ref: optionRef,
+    callback: closeOption
+  });
+
+  useEffect(() => {
+    setShowOption(isOpen);
+  }, [isOpen]);
+
+  return (
+    <div className='popover' ref={optionRef}>
+      <Button
+        variants='primary'
+        size='lg'
+        icon={icon}
+        additionalClass='btn-popover'
+        children={children}
+        onClick={handleTogglePopover}
+      />
+
+      {isShowOption && (
+        <div className='popover-option'>
+          {options.map((option, index) => (
+            <button
+              key={index}
+              className='btn-option'
+              type='button'
+              onClick={onOpenModal}
+            >
+              {option.text}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Popover;
