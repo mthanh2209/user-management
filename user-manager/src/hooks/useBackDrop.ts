48ff775afb2bfe407954dad54a4e9f68c78776e9
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-02 16:29:48 +0700

    Custom  hooks to use for popover component #6

---
+++ user-manager/src/hooks/useBackDrop.ts
@@ -0,0 +1,40 @@
+import {
+  useEffect,
+  RefObject
+} from 'react';
+
+interface IBackDropProps {
+  /**
+   * Reference to the HTMLElement used to determine clicks outside of its boundaries.
+   */
+  ref: RefObject<HTMLElement>
+  /**
+   * Callback function invoked when a click occurs outside the referenced element.
+   */
+  callback: () => void
+}
+
+/**
+ * Hook that handles click events outside a specified element.
+ * @param param0 - Object containing ref and callback properties.
+ */
+const useBackDrop = ({
+  ref,
+  callback
+}: IBackDropProps) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent): void => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [ref, callback]);
+};
+
+export default useBackDrop;
