30f7d97c618729cd54aa861228a5c25a7ae0fe48
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-16 13:50:02 +0700

    Implement switch status component #15

---
+++ user-manager/src/components/DataDisplay/SwitchStatus/SwitchStatus.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+// Components
+import SwitchStatus from '@components/DataDisplay/SwitchStatus/index';
+
+export default {
+  title: 'Components/SwitchStatus',
+  component: SwitchStatus,
+  tags: ['autodocs'],
+  argTypes: {
+    isChecked: { description: 'One boolean to test for active or notActive.' }
+  }
+} as Meta;
+type Story = StoryObj<typeof SwitchStatus>;
+
+export const Default: Story = {
+  args: {
+    isChecked: false
+  }
+};
