664c6ae5d968726a1c5fe902f1a45b5c6f6c9938
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-20 14:32:07 +0700

    Update storybook for profile editor component #21

--- user-manager/src/components/DataDisplay/ProfileEditor/ProfileEditor.stories.tsx
+++ user-manager/src/components/DataDisplay/ProfileEditor/ProfileEditor.stories.tsx
@@ -11,8 +11,43 @@ export default {
   component: ProfileEditor,
   tags: ['autodocs'],
   argTypes: {
-    tabs: {
-      description: 'List of tabs, each tab will contain different content.'
+    id: {
+      description: 'The unique identifier for the user profile.'
+    },
+    avatar: {
+      description: "The URL or path to the user's avatar image."
+    },
+    fullName: {
+      description: 'The full name of the user.'
+    },
+    email: {
+      description: 'The email address of the user.'
+    },
+    isActive: {
+      description:
+        'A boolean indicating whether the user profile is active or not.'
+    },
+    registeredDate: {
+      description: 'The date when the user registered.'
+    },
+    lastVisitedDate: {
+      description: 'The date when the user last visited.'
+    },
+    details: {
+      description: 'Additional details or description about the user.'
+    },
+    bgColor: {
+      description: 'The background color of the user profile.'
+    },
+    onSaveUser: {
+      description: 'Callback function triggered when the user profile is saved.'
+    },
+    onDeleteUser: {
+      description:
+        'Callback function triggered when the user profile is deleted.'
+    },
+    showToast: {
+      description: 'Function to show a toast or notification.'
     }
   }
 } as Meta;
