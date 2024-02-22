fe82447f83d022ac81c4d7677f7fa495ec2b9fbd
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-22 14:43:42 +0700

    Implement enum constant #79

--- user-manager/src/types/index.ts
+++ user-manager/src/types/index.ts
@@ -2,6 +2,3 @@ export type { IPopoverOption } from './interfaces/popoverOption';
 export type { IItemNav, ItemViewProps, ItemAssign } from './interfaces/items';
 export type { IColumnProps } from './interfaces/columns';
 export type { IUserProps } from './interfaces/users';
-
-export type AssignmentOptions = 'Assigned directly' | 'All assignments';
-export type SingleOptionTypes = 'Roles assigned';
