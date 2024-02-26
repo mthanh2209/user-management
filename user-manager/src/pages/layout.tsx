cc5b05f8c44a85d6884faae7fd376add0522e78f
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-26 17:29:46 +0700

    Implement add new user #25

--- user-manager/src/pages/layout.tsx
+++ user-manager/src/pages/layout.tsx
@@ -8,14 +8,57 @@ import Toast from '@components/DataDisplay/Toast';
 // Constants
 import { PATH, POPPER_OPTION } from '@constants';
 
+// Services
+import { addUser, getUsers } from '@services';
+
+// Types
+import { IUser } from '@types';
+
 const Layout = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [showSidebar, setShowSidebar] = useState(true);
   const [showToast, setShowToast] = useState({
     show: false,
     isError: false,
     key: 0
   });
+  const [selectedRow, setSelectedRow] = useState<{
+    index: number;
+    data: IUser | null;
+  }>({
+    index: 0,
+    data: null
+  });
 
-  const handleAddUser = () => {};
+  const handleShowToast = (show = true, isError = false) => {
+    setShowToast((prevToast) => ({
+      show,
+      isError,
+      key: prevToast.key + 1
+    }));
+  };
+
+  const handleAddUser = async (userName: string) => {
+    try {
+      const response = await addUser(userName);
+
+      if (response.data) {
+        const { data } = await getUsers();
+
+        setUsers(data);
+        setSelectedRow({
+          index: data.length,
+          data: data[data.length - 1]
+        });
+        setShowSidebar(true);
+        handleShowToast(true, false);
+      } else {
+        handleShowToast(true, true);
+      }
+    } catch (error) {
+      console.error('Unexpected error:', error);
+    }
+  };
 
   const navigate = useNavigate();
 
@@ -52,10 +95,7 @@ const Layout = () => {
       <header className='main-header'>
         User Manager
         {showToast.show && (
-          <Toast
-            isError={showToast.isError}
-            key={showToast.key}
-          />
+          <Toast isError={showToast.isError} key={showToast.key} />
         )}
       </header>
       <main className='main-body'>
