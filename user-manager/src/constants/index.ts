04ef0d51996ca732d85c93b10a8768e75169ff5e
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-26 11:03:57 +0700

    Create constants #24

--- user-manager/src/constants/index.ts
+++ user-manager/src/constants/index.ts
@@ -3,3 +3,5 @@ export { INFO_TYPE } from '@constants/types';
 export { TOAST_TYPE, LOADING } from '@constants/toast';
 export { AssignmentOptions, SingleOptionTypes } from '@constants/enum';
 export { API } from '@constants/url';
+export { POPPER_OPTION } from '@constants/popoverOption';
+export { NAVIGATION_ITEMS } from '@constants/navigation';
---
+++ user-manager/src/constants/navigation.ts
@@ -0,0 +1,14 @@
+export const NAVIGATION_ITEMS = [
+  {
+    id: 0,
+    label: 'users'
+  },
+  {
+    id: 1,
+    label: 'roles'
+  },
+  {
+    id: 2,
+    label: 'rules'
+  }
+];
