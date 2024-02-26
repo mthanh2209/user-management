3a7c2d6761c7ac0516ff81ed6fa737dc553e2560
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-26 14:50:49 +0700

    Set up react router #87

--- user-manager/src/App.tsx
+++ user-manager/src/App.tsx
@@ -1,5 +1,17 @@
 import '@App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App = () => {};
+// Routes
+import AppRoutes from '@routes';
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </>
+  );
+};
 
 export default App;
---
+++ user-manager/src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
+// Pages
+import { HomePage, Layout, RolePage, RulePage } from '@pages';
+
+// Constants
+import { PATH } from '@constants';
+
+const AppRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route path={PATH.HOME_PATH} element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path={PATH.ROLES_PATH} element={<RolePage />} />
+          <Route path={PATH.RULES_PATH} element={<RulePage />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default AppRoutes;
