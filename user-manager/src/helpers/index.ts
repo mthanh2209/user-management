b9c6b5df0606105842442dd6c2f284c872f2e503
mthanh2209 <mongthanh.220902@gmail.com>
2024-02-27 10:16:46 +0700

    Create validation for input modal #25

--- user-manager/src/helpers/index.ts
+++ user-manager/src/helpers/index.ts
@@ -3,7 +3,11 @@ export { convertToDataURL } from '@helpers/convertURL';
 export { getRandomColor } from '@helpers/getRandomColor';
 export { getObjValue } from '@helpers/getObjValue';
 export { formatDate } from '@helpers/formatDate';
-export { isEmailValid, isFullNameValid } from '@helpers/validation';
+export {
+  isEmailValid,
+  isFullNameValid,
+  isModalInputValid 
+} from '@helpers/validation';
 export { fetcher } from '@helpers/fetcher';
 export { filterUsers } from '@helpers/filterUsers';
 export { highlightKeyword } from '@helpers/highlightKeyword';
