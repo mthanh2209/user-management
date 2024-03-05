aa0bdd00121f5cfe12a9a1a69c59677c7fce7489
mthanh2209 <mongthanh.220902@gmail.com>
2024-03-05 16:40:53 +0700

    Create service to post new role #36

--- user-manager/src/helpers/generateNewObj.ts
+++ user-manager/src/helpers/generateNewObj.ts
@@ -14,6 +14,19 @@ export const generateNewUser = (fullName: string) => {
     registeredDate: formatDate(new Date().toString()),
     lastVisitedDate: null,
     details: '',
-    bgColor: getRandomColor(),
+    bgColor: getRandomColor()
+  };
+};
+
+/**
+ * Generates a new role object with default or provided information.
+ * @param name - The name of the new role.
+ * @returns An object representing a new role with default or initialized values.
+ */
+export const generateNewRole = (name: string) => {
+  return {
+    avatar: '',
+    name,
+    bgColor: getRandomColor()
   };
 };
