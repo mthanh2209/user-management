2dc529a066cb4eb10948442bdd012d90cc76f49d
mthanh2209 <mongthanh.220902@gmail.com>
2024-03-06 16:01:28 +0700

    Implement loading component #123

---
+++ user-manager/src/components/Loading/index.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+// CSS
+import '@components/Loading/Loading.css';
+
+// Constants
+import { LOADING } from '@constants';
+
+interface ILoadingProp {
+  isProcessing: boolean;
+}
+
+const Loading = ({ isProcessing }: ILoadingProp) => {
+  const [showLoading, setShowLoading] = useState(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (!isProcessing) {
+      setShowLoading(false);
+      return;
+    }
+
+    setShowLoading(true);
+    timer = setTimeout(() => {
+      setShowLoading(false);
+    }, LOADING.TIMER_LOADING);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isProcessing]);
+
+  return (
+    <>
+      {showLoading && isProcessing && (
+        <div className='loading-wrapper'>
+          <span className='loading-icon' />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Loading;
