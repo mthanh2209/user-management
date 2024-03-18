11666e35eac9f67b05b1224fafb8aae9e0523569
mthanh2209 <mongthanh.220902@gmail.com>
2024-03-18 15:26:05 +0700

    Delete unused helper #148

--- user-manager/src/helpers/index.ts
+++ user-manager/src/helpers/index.ts
@@ -1,7 +1,6 @@
 export { capitalizeLetter } from '@helpers/capitalizeLetter';
 export { convertToDataURL } from '@helpers/convertURL';
 export { getRandomColor } from '@helpers/getRandomColor';
-export { getObjValue } from '@helpers/getObjValue';
 export { formatDate } from '@helpers/formatDate';
 export {
   isEmailValid,
